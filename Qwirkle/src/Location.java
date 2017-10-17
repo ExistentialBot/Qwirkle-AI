564155c6df8971ee981b3c6e7c0ba9886f9ff986
KarimAK99 <KarimAbdelKader@outlook.com>
2017-10-17 16:38:55 +0200

    added stuff
    classes me and nino made that were missing from tommy's code

---
+++ Qwirkle/src/Location.java
@@ -0,0 +1,26 @@
+
+public class Location {
+
+	public Location(int r, int c){
+		
+		row = r;
+		coloumn = c;
+	}
+	
+	int row;
+	int coloumn;
+	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColoumn() {
+		return coloumn;
+	}
+	public void setColoumn(int coloumn) {
+		this.coloumn = coloumn;
+	}
+	
+}
